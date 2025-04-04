dabe332c8d11cc55265332eb87bede6569b91318
ATHUL-MURALI <anithaathul916@gmail.com>
2025-04-05 00:28:22 +0530

    stuff

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -10,6 +10,8 @@ import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
 import { fadeAnimation, slideAnimation } from '../config/motion';
 import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
+// export const AddVton = 0;// my variable to check if the admin page is loaded from 3d site
+
 const Customizer = () => {
   const snap = useSnapshot(state);
 
@@ -82,15 +84,28 @@ const Customizer = () => {
     }
   };
 
+  // const handleDecals = (type, result) => {
+  //   const decalType = DecalTypes[type];
+
+  //   state[decalType.stateProperty] = result;
+
+  //   if (!activeFilterTab[decalType.filterTab]) {
+  //     handleActiveFilterTab(decalType.filterTab);
+  //   }
+  // };
+
   const handleDecals = (type, result) => {
     const decalType = DecalTypes[type];
-
+  
     state[decalType.stateProperty] = result;
-
+  
+    imageDataStore.ImgData = result; // <-- Save image globally
+  
     if (!activeFilterTab[decalType.filterTab]) {
       handleActiveFilterTab(decalType.filterTab);
     }
   };
+
 
   const handleActiveFilterTab = (tabName) => {
     switch (tabName) {
@@ -166,8 +181,8 @@ const Customizer = () => {
                 />
               ))}
             </motion.div>
-{/*ivide                                                                                                           */}
-            <div className="canvas-container relative">
+{/*Download button                                                                                                           */}
+            {/* <div className="canvas-container relative">
             <button className='download-btn absolute bottom-4 right-4 bg-white p-2 rounded shadow z-50'
               onClick={downloadCanvasToImage}>
               <img
@@ -176,8 +191,29 @@ const Customizer = () => {
                 className='w-3/5 h-3/5 object-contain'
               />
             </button>
+            </div> */}
+
+            <div className="canvas-container relative">
+              <button
+                className='download-btn absolute bottom-4 right-4 bg-white p-2 rounded shadow z-50'
+                onClick={() => {
+                  downloadCanvasToImage();
+
+                  // Optionally store canvas snapshot here too if needed
+                  // For now, we already have image saved to imageDataStore.ImgData
+
+                  window.location.href = 'http://localhost:5174/add';
+                }}
+              >
+                <img
+                  src={download}
+                  alt='download_image'
+                  className='w-3/5 h-3/5 object-contain'
+                />
+              </button>
             </div>
-{/*ivide                                                                                                           */}
+
+{/*Download button                                                                                                         */}
           </>      
         )}
       </AnimatePresence>
@@ -188,197 +224,3 @@ const Customizer = () => {
 export default Customizer;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState, useEffect } from 'react'
-// import { AnimatePresence, motion } from 'framer-motion';
-// import {useSnapshot} from 'valtio';
-
-// import config from '../config/config';
-// import state from '../store';
-// import { download, logoShirt } from '../assets';
-// import { downloadCanvasToImage, reader } from '../config/helpers';
-// import { EditorTabs, FilterTabs, DecalTypes} from '../config/constants';
-// import { fadeAnimation, slideAnimation } from '../config/motion';
-// import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
-
-// const Customizer = () => {
-//   const snap = useSnapshot(state);
-  
-//   const [file, setFile] = useState('');
-
-//   const [prompt, setPrompt] = useState('');
-//   const [generatingImg,setGeneratingImg] = useState(false);
-
-//   const [activeEditorTab,setActiveEditorTab] = useState("");
-//   const [activeFilterTab,setActiveFilterTab] = useState({
-//     logoShirt: true,
-//     stylishShirt: false,
-//   })
-
-
-
-//   const generateTabContent = () => {
-//     switch (activeEditorTab){
-//       case "colorpicker":
-//         return <ColorPicker />
-//       case "filepicker":
-//         return <FilePicker 
-//           file={file}
-//           setFile={setFile}
-//           readFile={readFile}
-//         />
-//       case "aipicker":
-//         return <AIPicker 
-//           prompt={prompt}
-//           setPrompt={setPrompt}
-//           generatingImg={generatingImg}
-//           handleSubmit={handleSubmit}
-//         />
-//       default:
-//         return null;
-//     }
-//   }
-
-//   const handleSubmit = async(type) => {
-//     if(!prompt) return alert("please enter a prompt");
-
-//     try{
-//       setGeneratingImg(true)
-
-//       const response = await fetch('http://localhost:8080/api/v1/dalle', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({
-//           prompt,
-//         })
-//       })
-
-//       const data = await response.json();
-
-//       handleDecals(type, `data:image/png;base64,${data.photo}`)
-//     } catch (error) {
-//       alert(error)
-//     } finally {
-//       setGeneratingImg(false);
-//       setActiveEditorTab("");
-//     }
-//   }
-
-//   const handleDecals = (type, result) => {
-//     const decalType = DecalTypes[type];
-
-//     state[decalType.stateProperty] = result;
-
-//     if(!activeFilterTab[decalType.filterTab]){
-//       handleActiveFilterTab(decalType.filterTab)
-//     }
-//   }
-
-//   const handleActiveFilterTab = (tabName) => {
-//     switch(tabName){
-//       case "logoShirt":
-//           state.isLogoTexture = !activeFilterTab[tabName];
-//         break;
-//       case "stylishShirt":
-//           state.isFullTexture = !activeFilterTab[tabName];
-//         break;
-//       default:
-//         state.isLogoTexture = true;
-//         state.isFullTexture = false;
-//         break;
-//     }
-
-//     //after setting the state, activeFilterTab is updated
-    
-//     setActiveFilterTab((prevState) => {
-//       return {
-//         ...prevState,
-//         [tabName]: !prevState[tabName]
-//       }
-//     })
-//   }
-
-//   const readFile = (type) => {
-//     reader(file)
-//       .then((result) => {
-//         handleDecals(type, result);
-//         setActiveEditorTab("");
-//       })
-//   }
-
-//   return (
-//     <div>
-//       <AnimatePresence>
-//         {!snap.intro && (
-//           <>
-//             <motion.div
-//              key="custom"
-//              className='absolute top-80 left-0 z-10'
-//              {...slideAnimation('left')} 
-//             >
-//               <div className='flix item-center min-h-screen'>
-//                 <div className='editortabs-container tabs'>
-//                   {EditorTabs.map((tab) => (
-//                     <Tab 
-//                       key={tab.name}
-//                       tab={tab}
-//                       handleClick={() => setActiveEditorTab(tab.name)}
-//                     />
-//                   ))}
-
-//                   {generateTabContent()}
-//                 </div>
-//               </div>
-//             </motion.div>
-
-//             <motion.div
-//               className="absolute z-10 top-5 right-5"
-//               {...fadeAnimation}
-//             >
-//               <CustomButton
-//                 type="filled"
-//                 title="Go back"
-//                 handleClick={() => state.intro = true}
-//                 customStyles="w-fit px-4 py-2.5 font-bold text-sm"
-
-//               />
-//             </motion.div>
-
-//             <motion.div
-//               className='filtertabs-container'
-//               {...slideAnimation('up')}
-//             >
-//               {FilterTabs.map((tab) => (
-//                 <Tab 
-//                   key={tab.name}
-//                   tab={tab}
-//                   isFilterTab
-//                   isActiveTab={activeFilterTab[tab.name]}
-//                   handleClick={() => handleActiveFilterTab(tab.name)}
-//                 />
-//               ))}            
-//              </motion.div>
-//           </>
-//         )}
-//       </AnimatePresence>
-//     </div>
-//   )
-// }
-
-// export default Customizer
