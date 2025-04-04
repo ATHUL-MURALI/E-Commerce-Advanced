c252cb0db6b41715c3e19be52f5a357f3adc4698
ATHUL-MURALI <anithaathul916@gmail.com>
2025-04-05 01:09:15 +0530

    webscraping

--- FineTuning StableD/preprocess.py
+++ FineTuning StableD/preprocess.py
@@ -1,14 +1,14 @@
 import os
 from PIL import Image
 
-input_folder = "logos_dataset/"
-output_folder = "logos_resized/"
+input_folder = "FineTuning StableD/logos_dataset/"
+output_folder = "FineTuning StableD/logos_resized/"
+
 os.makedirs(output_folder, exist_ok=True)
 
-for root, _, files in os.walk(input_folder):
-    for file in files:
-        if file.endswith((".png", ".jpg", ".jpeg")):
-            img_path = os.path.join(root, file)
-            img = Image.open(img_path).convert("RGB")
-            img = img.resize((512, 512))  # Resize to 512x512
-            img.save(os.path.join(output_folder, file))
+for file in os.listdir(input_folder):
+    if file.endswith((".png", ".jpg", ".jpeg")):
+        img_path = os.path.join(input_folder, file)
+        img = Image.open(img_path).convert("RGB")
+        img = img.resize((512, 512))  # Resize to 512x512
+        img.save(os.path.join(output_folder, file))
